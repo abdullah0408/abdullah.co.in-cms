f83fce5e142159f80495bb133d502abdf14278c2
abdullah <abdullah981066@gmail.com>
2025-12-21 04:42:31 +0530

    feat: add imagekit plugin

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,43 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "ik.imagekit.io", // ImageKit domain for images, add your custom domain if you use one
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "ik.imagekit.io", // ImageKit domain for videos/audio, add your custom domain if you use one
+          ],
+          "frame-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "eml.imagekit.io", // For ImageKit UI components
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
